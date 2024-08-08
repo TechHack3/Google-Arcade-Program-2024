7d06d688b9fa9429da0dc46c11c71c0b4486bf39
TechHack3 <140188790+TechHack3@users.noreply.github.com>
2024-08-08 10:55:53 +0530

    Update quicklabgsp416.sh

--- Working with JSON Arrays and Structs in BigQuery/quicklabgsp416.sh
+++ Working with JSON Arrays and Structs in BigQuery/quicklabgsp416.sh
@@ -200,7 +200,7 @@ Lab Completed !!!
 ${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
-read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+read -p "${BOLD}${RED}Subscribe to Code with TechHack [y/n] : ${RESET}" CONSENT_REMOVE
 
 while [ "$CONSENT_REMOVE" != 'y' ]; do
   sleep 10
