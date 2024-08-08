5f0cbdea0cbc1aaa8ef2189ffc0e024d028459b1
TechHack3 <140188790+TechHack3@users.noreply.github.com>
2024-08-08 10:57:00 +0530

    Update quicklabgsp087.sh

--- Autoscaling an Instance Group with Custom Cloud Monitoring Metrics/quicklabgsp087.sh
+++ Autoscaling an Instance Group with Custom Cloud Monitoring Metrics/quicklabgsp087.sh
@@ -72,11 +72,11 @@ Lab Completed !!!
 ${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
-read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+read -p "${BOLD}${RED}Subscribe to Code With TechHack [y/n] : ${RESET}" CONSENT_REMOVE
 
 while [ "$CONSENT_REMOVE" != 'y' ]; do
   sleep 10
-  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+  read -p "${BOLD}${YELLOW}Do Subscribe to Code With TechHack [y/n] : ${RESET}" CONSENT_REMOVE
 done
 
 echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
